8fea377ea83c170f1b394a5fa0b64c86c8d9a15b
Sincere Banks <abanks7296@gmail.com>
2023-10-03 15:30:14 -0400

    update

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import os
 
 app = Flask(__name__)
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
-
 @app.route('/')
 def index():
     return 'Hello, World!'
@@ -42,7 +39,8 @@ def process_dropdown():
         return "Awww"
 
 if __name__ == '__main__':
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=False)
 
 
