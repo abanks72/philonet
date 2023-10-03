d41b821cce648dde374bb444656431f49e028b27
Sincere Banks <abanks7296@gmail.com>
2023-10-03 14:59:25 -0400

    update

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def process_dropdown():
         return "Awww"
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=False)
 
 
--- apptest.py
+++ apptest.py
@@ -1,4 +1,7 @@
-import requests
+from flask import Flask
+import requests, os
+
+app = Flask(__name__)
 
 # Set the URL of your Heroku app
 heroku_app_url = "https://philonet-7f9320692cfa.herokuapp.com/"  # Replace with your app's URL
@@ -13,3 +16,8 @@ if response.status_code == 200:
     print(response.text)
 else:
     print(f"Failed to access the app. Status code: {response.status_code}")
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    app.run(debug=False)
